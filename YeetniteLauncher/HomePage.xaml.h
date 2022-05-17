7f4073623e03de4d119babe294a593dc5a8a646e
Antonios Papadakis <bdpaboi123@gmail.com>
2022-05-17 08:27:04 -0500

    Add Login Page And Navigation to Home Page

---
+++ YeetniteLauncher/HomePage.xaml.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "HomePage.g.h"
+
+namespace winrt::YeetniteLauncher::implementation
+{
+    struct HomePage : HomePageT<HomePage>
+    {
+        HomePage();
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::YeetniteLauncher::factory_implementation
+{
+    struct HomePage : HomePageT<HomePage, implementation::HomePage>
+    {
+    };
+}
---
+++ YeetniteLauncher/LoginPage.xaml.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "LoginPage.g.h"
+
+namespace winrt::YeetniteLauncher::implementation
+{
+    struct LoginPage : LoginPageT<LoginPage>
+    {
+        LoginPage();
+        Windows::Foundation::IAsyncAction Login(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::YeetniteLauncher::factory_implementation
+{
+    struct LoginPage : LoginPageT<LoginPage, implementation::LoginPage>
+    {
+    };
+}
