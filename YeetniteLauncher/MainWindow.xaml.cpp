f8a2fffdfa19aa0490ada6425bdc2bc556f36ce3
Antonios Papadakis <bdpaboi123@gmail.com>
2022-06-10 21:37:30 -0500

    BROKEN - Auto-Redirect Support in Login
    Auto-redirect the user to the home page if they've already logged in

--- YeetniteLauncher/MainWindow.xaml.cpp
+++ YeetniteLauncher/MainWindow.xaml.cpp
@@ -6,6 +6,7 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Windows::Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -15,6 +16,17 @@ namespace winrt::YeetniteLauncher::implementation
 	MainWindow::MainWindow()
 	{
 		InitializeComponent();
-		rootFrame().Navigate(xaml_typename<LoginPage>());
+		try {
+			auto username = winrt::unbox_value<winrt::hstring>(ApplicationData::Current().LocalSettings().Values().Lookup(L"Username")).c_str();
+			if (username != nullptr)
+				rootFrame().Navigate(xaml_typename<HomePage>());
+			else
+				rootFrame().Navigate(xaml_typename<LoginPage>());
+		}
+		catch (winrt::hresult_no_interface e) {
+			OutputDebugString(L"We failed again:\n\n");
+			OutputDebugString(e.message().c_str());
+			rootFrame().Navigate(xaml_typename<LoginPage>());
+		}
 	}
 }
